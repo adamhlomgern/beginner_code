fac170ad199405533ccf0a623f977c60bf985ad3
adamhlomgern <146996877+adamhlomgern@users.noreply.github.com>
2024-06-08 12:41:16 +0200

    format_name

--- doodles.py
+++ doodles.py
@@ -1,4 +1,13 @@
 #Functions with outputs
 
+
 def format_name():
+    """Take a first and last name and format it to return the title case version of the name.                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         """
+    f_name = input("First name: ")
+    l_name = input("Last name: ")
+    formatted_f_name = f_name.title()
+    formatted_l_name = l_name.title()
+    return f"{formatted_f_name} {formatted_l_name}"
     
+formatted_name = format_name()
+print(formatted_name)
